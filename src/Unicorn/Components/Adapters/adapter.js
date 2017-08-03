0aadfd5163008cda0cf525c083b104a5ba6c5e6f
tung1147 <tung7749@gmail.com>
2017-08-03 17:04:17 +0700

    update adpter

--- src/Unicorn/Components/Adapters/adapter.js
+++ src/Unicorn/Components/Adapters/adapter.js
@@ -1,3 +1,14 @@
 /**
  * Created by Admin on 8/3/2017.
  */
+/**
+ * Created by Admin on 8/3/2017.
+ */
+(function () {
+
+    var adapter = uc.adapter = function () {}
+
+    adapter.prototype = new EventEmitter();
+
+
+}.call(this));
--- src/Unicorn/Components/Adapters/index.js
+++ src/Unicorn/Components/Adapters/index.js
@@ -1,3 +1,13 @@
 /**
  * Created by Admin on 8/3/2017.
  */
+(function () {
+    var Adapters = {
+        "lobby" : 0
+    }
+
+    var adapter = uc.adapterManager = new EventEmitter();
+
+    adapter._adapters = [];
+
+}.call(this));
